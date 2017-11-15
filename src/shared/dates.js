8835aa5c3f6430e4296bc251cce09c1c9b113581
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-11-15 23:13:16 +0100

    Update react-calendar dependency, use all functions from shared dates util from react-calendar

--- src/shared/dates.js
+++ src/shared/dates.js
@@ -1,60 +1,23 @@
 import {
   getYear,
+  getMonth,
   getMonthIndex,
   getDay,
   getDaysInMonth,
   getBegin,
   getEnd,
+  getISOLocalMonth,
+  getISOLocalDate,
 } from 'react-calendar/build/shared/dates';
 
 export {
   getYear,
+  getMonth,
   getMonthIndex,
   getDay,
   getDaysInMonth,
   getBegin,
   getEnd,
-};
-
-/* Simple getters - getting a property of a given point in time */
-
-export const getMonth = date => date.getMonth() + 1;
-
-/* Complex getters - getting a property somehow related to a given point in time */
-
-/**
- * Returns local month in ISO-like format (YYYY-MM).
- */
-export const getISOLocalMonth = (value) => {
-  if (!value) {
-    return value;
-  }
-
-  if (!(value instanceof Date)) {
-    throw new Error(`Invalid date: ${value}`);
-  }
-
-  const year = getYear(value);
-  const month = `0${getMonth(value)}`.slice(-2);
-
-  return `${year}-${month}`;
-};
-
-/**
- * Returns local date in ISO-like format (YYYY-MM-DD).
- */
-export const getISOLocalDate = (value) => {
-  if (!value) {
-    return value;
-  }
-
-  if (!(value instanceof Date)) {
-    throw new Error(`Invalid date: ${value}`);
-  }
-
-  const year = getYear(value);
-  const month = `0${getMonth(value)}`.slice(-2);
-  const day = `0${getDay(value)}`.slice(-2);
-
-  return `${year}-${month}-${day}`;
+  getISOLocalMonth,
+  getISOLocalDate,
 };
