c6887c35185fff7cb5ab2b195b4aead52db171c4
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-12-17 00:56:27 +0100

    Reorder imports

--- src/DatePicker.jsx
+++ src/DatePicker.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import mergeClassNames from 'merge-class-names';
+import detectElementOverflow from 'detect-element-overflow';
 
 import Calendar from 'react-calendar/build/entry.nostyle';
-
-import detectElementOverflow from 'detect-element-overflow';
 
 import DateInput from './DateInput';
 
