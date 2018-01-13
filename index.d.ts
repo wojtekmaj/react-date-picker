85188b0ec050924c3f3d64e2409ba4c96373b01e
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-01-13 15:45:10 +0100

    Remove unused types, add className to make it clear it's RDP's own prop

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,14 @@
-import {CalendarProps} from "react-calendar";
-
+import { CalendarProps } from "react-calendar";
 
 declare module "react-date-picker" {
-  type Detail = "month" | "year" | "decade" | "century"
-
   export default function DatePicker(props: DatePickerProps): JSX.Element;
 
   export interface DatePickerProps extends CalendarProps {
     calendarClassName?: string | string[];
     calendarIcon?: JSX.Element;
+    className?: string | string[];
     clearIcon?: JSX.Element;
     isOpen?: boolean;
-  }
-
-  export interface CalendarTileProperties {
-    date: Date;
-    view: Detail;
-  }
-
-  export interface ViewCallbackProperties {
-    activeStartDate: Date;
-    view: Detail;
+    returnValue?: "start" | "end";
   }
 }
