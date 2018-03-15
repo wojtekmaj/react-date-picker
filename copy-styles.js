30e583f5b46e492d0aa56ce79f56bfe02264d18d
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-03-15 21:50:50 +0100

    Simplify building styles process

--- copy-styles.js
+++ copy-styles.js
@@ -7,11 +7,3 @@ fs.copyFile('src/DatePicker.less', 'dist/DatePicker.less', (error) => {
   // eslint-disable-next-line no-console
   console.log('DatePicker.less copied successfully.');
 });
-
-fs.copyFile('src/DatePicker.css', 'dist/DatePicker.css', (error) => {
-  if (error) {
-    throw error;
-  }
-  // eslint-disable-next-line no-console
-  console.log('DatePicker.css copied successfully.');
-});
