080f86cde8f68a81c54441373c1b1048182517bc
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-04-03 19:55:52 +0200

    Simplify findPreviousInput/findNextInput functions

--- src/DateInput.jsx
+++ src/DateInput.jsx
@@ -106,21 +106,17 @@ const getDetailValueArray = (value, minDate, maxDate, maxDetail) => {
 
 const isValidInput = element => element.tagName === 'INPUT' && element.type === 'number';
 
-const findPreviousInput = (element) => {
-  let previousElement = element;
-  do {
-    previousElement = previousElement.previousElementSibling;
-  } while (previousElement && !isValidInput(previousElement));
-  return previousElement;
-};
-
-const findNextInput = (element) => {
+const findInput = (element, property) => {
   let nextElement = element;
   do {
-    nextElement = nextElement.nextElementSibling;
+    nextElement = nextElement[property];
   } while (nextElement && !isValidInput(nextElement));
   return nextElement;
 };
+
+const findPreviousInput = element => findInput(element, 'previousElementSibling');
+
+const findNextInput = element => findInput(element, 'nextElementSibling');
 
 const focus = element => element && element.focus();
 
