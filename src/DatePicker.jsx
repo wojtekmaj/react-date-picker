0966e6d91f7a8a17bac8307e63396f666045bc8e
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-07-12 02:12:20 +1000

    Add support for disableCalendar prop

--- src/DatePicker.jsx
+++ src/DatePicker.jsx
@@ -121,6 +121,7 @@ export default class DatePicker extends PureComponent {
       clearAriaLabel,
       clearIcon,
       dayAriaLabel,
+      disableCalendar,
       disabled,
       format,
       locale,
@@ -178,7 +179,7 @@ export default class DatePicker extends PureComponent {
             {clearIcon}
           </button>
         )}
-        {calendarIcon !== null && (
+        {calendarIcon !== null && !disableCalendar && (
           <button
             aria-label={calendarAriaLabel}
             className={`${baseClassName}__calendar-button ${baseClassName}__button`}
@@ -196,9 +197,10 @@ export default class DatePicker extends PureComponent {
   }
 
   renderCalendar() {
+    const { disableCalendar } = this.props;
     const { isOpen } = this.state;
 
-    if (isOpen === null) {
+    if (isOpen === null || disableCalendar) {
       return null;
     }
 
@@ -311,6 +313,7 @@ DatePicker.propTypes = {
   clearAriaLabel: PropTypes.string,
   clearIcon: PropTypes.node,
   dayAriaLabel: PropTypes.string,
+  disableCalendar: PropTypes.bool,
   disabled: PropTypes.bool,
   format: PropTypes.string,
   isOpen: PropTypes.bool,
--- src/__tests__/DatePicker.jsx
+++ src/__tests__/DatePicker.jsx
@@ -163,6 +163,18 @@ describe('DatePicker', () => {
     expect(calendar).toHaveLength(1);
   });
 
+  it('does not render Calendar component when given disableCalendar & isOpen flags', () => {
+    const component = mount(
+      <DatePicker disableCalendar isOpen />
+    );
+
+    const dateInput = component.find('DateInput');
+    const calendar = component.find('Calendar');
+
+    expect(dateInput).toHaveLength(1);
+    expect(calendar).toHaveLength(0);
+  });
+
   it('opens Calendar component when given isOpen flag by changing props', () => {
     const component = mount(
       <DatePicker />
