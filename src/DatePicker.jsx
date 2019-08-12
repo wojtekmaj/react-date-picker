56def41c7369018bf30da4928c80c1c9e3388ad3
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-08-12 13:15:55 +0200

    Enable & enforce react/sort-comp ESLint rule

--- src/DatePicker.jsx
+++ src/DatePicker.jsx
@@ -30,10 +30,6 @@ export default class DatePicker extends PureComponent {
 
   state = {};
 
-  get eventProps() {
-    return makeEventProps(this.props);
-  }
-
   componentDidMount() {
     this.handleOutsideActionListeners();
   }
@@ -52,36 +48,14 @@ export default class DatePicker extends PureComponent {
     this.handleOutsideActionListeners(false);
   }
 
-  handleOutsideActionListeners(shouldListen) {
-    const { isOpen } = this.state;
-
-    const shouldListenWithFallback = typeof shouldListen !== 'undefined' ? shouldListen : isOpen;
-    const fnName = shouldListenWithFallback ? 'addEventListener' : 'removeEventListener';
-    outsideActionEvents.forEach(eventName => document[fnName](eventName, this.onOutsideAction));
+  get eventProps() {
+    return makeEventProps(this.props);
   }
 
   onOutsideAction = (event) => {
     if (this.wrapper && !this.wrapper.contains(event.target)) {
       this.closeCalendar();
     }
-  }
-
-  openCalendar = () => {
-    this.setState({ isOpen: true });
-  }
-
-  closeCalendar = () => {
-    this.setState((prevState) => {
-      if (!prevState.isOpen) {
-        return null;
-      }
-
-      return { isOpen: false };
-    });
-  }
-
-  toggleCalendar = () => {
-    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
   }
 
   onChange = (value, closeCalendar = true) => {
@@ -110,9 +84,35 @@ export default class DatePicker extends PureComponent {
     this.openCalendar();
   }
 
+  openCalendar = () => {
+    this.setState({ isOpen: true });
+  }
+
+  closeCalendar = () => {
+    this.setState((prevState) => {
+      if (!prevState.isOpen) {
+        return null;
+      }
+
+      return { isOpen: false };
+    });
+  }
+
+  toggleCalendar = () => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  }
+
   stopPropagation = event => event.stopPropagation();
 
   clear = () => this.onChange(null);
+
+  handleOutsideActionListeners(shouldListen) {
+    const { isOpen } = this.state;
+
+    const shouldListenWithFallback = typeof shouldListen !== 'undefined' ? shouldListen : isOpen;
+    const fnName = shouldListenWithFallback ? 'addEventListener' : 'removeEventListener';
+    outsideActionEvents.forEach(eventName => document[fnName](eventName, this.onOutsideAction));
+  }
 
   renderInputs() {
     const {
