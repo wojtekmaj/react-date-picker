85533abd4237e54f8ede9f5d7b45aaffe9dbb451
Linus Marco <linusmarco@users.noreply.github.com>
2019-08-22 14:49:55 -0400

    Update MonthSelect to use placeholder prop

--- src/DateInput/MonthSelect.jsx
+++ src/DateInput/MonthSelect.jsx
@@ -17,6 +17,7 @@ export default function MonthSelect({
   short,
   year,
   value,
+  placeholder,
   ...otherProps
 }) {
   const maxMonth = min(12, maxDate && year === getYear(maxDate) && getMonth(maxDate));
@@ -43,7 +44,7 @@ export default function MonthSelect({
     >
       {!value && (
         <option value="">
-          --
+          {placeholder !== void 0 ? placeholder : '--'}
         </option>
       )}
       {dates.map((date) => {
