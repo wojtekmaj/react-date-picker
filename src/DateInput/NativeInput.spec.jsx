5584653b7c26087fd81e907ae82fd0997d9dd090
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-10-19 21:01:05 +0200

    Move tests to *.spec.js (#201)
    * Move tests to *.spec.js
    
    * Update .codeclimate.yml

--- src/DateInput/NativeInput.spec.jsx
+++ src/DateInput/NativeInput.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import NativeInput from '../NativeInput';
+import NativeInput from './NativeInput';
 
 /* eslint-disable comma-dangle */
 
--- src/DateInput/YearInput.spec.jsx
+++ src/DateInput/YearInput.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import YearInput from '../YearInput';
+import YearInput from './YearInput';
 
 /* eslint-disable comma-dangle */
 
