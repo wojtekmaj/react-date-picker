849b0d81c85e0e239897bc012226cf89e93d67bb
rojay simpson <harrysmith1991@gmail.com>
2019-12-06 03:31:38 -0500

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -4,20 +4,36 @@ declare module "react-date-picker" {
   export default function DatePicker(props: DatePickerProps): JSX.Element;
 
   export interface DatePickerProps extends CalendarProps {
+    autoFocus?: boolean;
+    calendarAriaLabel?: string;
     calendarClassName?: string | string[];
-    calendarIcon?: React.ReactElement;
-    className?: string | string[];
+    calendarIcon?: string | React.ReactNode;
+    className?: string;
     clearAriaLabel?: string;
-    clearIcon?: React.ReactElement;
-    clockAriaLabel?: string;
+    clearIcon?: string | React.ReactNode;
     dayAriaLabel?: string;
+    dayPlaceholder?: string;
     disabled?: boolean;
+    disableCalendar?: boolean;
     format?: string;
     isOpen?: boolean;
+    locale?: string;
+    maxDate?: Date;
+    maxDetail?: "month" | "year" | "decade" | "century";
+    minDate?: Date;
+    minDetail?: "month" | "year" | "decade" | "century";
     monthAriaLabel?: string;
+    monthPlaceholder?: string;
     name?: string;
     nativeInputAriaLabel?: string;
+    onCalendarClose?: () => void;
+    onCalendarOpen?: () => void;
+    onChange: (value: Date) => void;
     required?: boolean;
+    returnValue?: "start" | "end" | "range";
     showLeadingZeros?: boolean;
+    value?: Date | Date[];
+    yearAriaLabel?: string;
+    yearPlaceholder?: string;
   }
 }
