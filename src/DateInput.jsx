a67ef05067616781c24ec215ee493986ac638e11
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-09-11 21:41:04 +0200

    Fix Buddhist year not properly recognized in formatted date (#302)
    Fixes #294

--- src/DateInput.jsx
+++ src/DateInput.jsx
@@ -217,13 +217,6 @@ export default class DateInput extends PureComponent {
     return getFormatter(options);
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  get formatNumber() {
-    const options = { useGrouping: false };
-
-    return getFormatter(options);
-  }
-
   /**
    * Gets current value in a desired format.
    */
@@ -262,13 +255,23 @@ export default class DateInput extends PureComponent {
     const day = 11;
 
     const date = new Date(year, monthIndex, day);
+    const formattedDate = this.formatDate(locale, date);
 
-    return (
-      this.formatDate(locale, date)
-        .replace(this.formatNumber(locale, year), 'y')
-        .replace(this.formatNumber(locale, monthIndex + 1), 'M')
-        .replace(this.formatNumber(locale, day), 'd')
-    );
+    const datePieces = ['year', 'month', 'day'];
+    const datePieceReplacements = ['y', 'M', 'd'];
+
+    function formatDatePiece(name, dateToFormat) {
+      return getFormatter({ useGrouping: false, [name]: 'numeric' })(locale, dateToFormat).match(/\d{1,}/);
+    }
+
+    let placeholder = formattedDate;
+    datePieces.forEach((datePiece, index) => {
+      const formattedDatePiece = formatDatePiece(datePiece, date);
+      const datePieceReplacement = datePieceReplacements[index];
+      placeholder = placeholder.replace(formattedDatePiece, datePieceReplacement);
+    });
+
+    return placeholder;
   }
 
   get commonInputProps() {
