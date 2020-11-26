0ce0e97a9bb1c20fb3a18a66103809bf66b68200
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-11-26 10:44:24 +0100

    Update sample

---
+++ sample/Sample.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-date-picker';
+
+import './Sample.less';
+
+export default function Sample() {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div className="Sample">
+      <header>
+        <h1>react-date-picker sample page</h1>
+      </header>
+      <div className="Sample__container">
+        <main className="Sample__container__content">
+          <DatePicker
+            calendarAriaLabel="Toggle calendar"
+            clearAriaLabel="Clear value"
+            dayAriaLabel="Day"
+            monthAriaLabel="Month"
+            nativeInputAriaLabel="Date"
+            onChange={onChange}
+            value={value}
+            yearAriaLabel="Year"
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
