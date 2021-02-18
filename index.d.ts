b5b77aad365eb45904e5fc24d05316352aa33530
Wojciech Maj <kontakt@wojtekmaj.pl>
2021-02-18 22:20:27 +0100

    Add ARIA labels to TypeScript definitions
    Co-Authored-By: tenKinetic <1566738+tenKinetic@users.noreply.github.com>

--- index.d.ts
+++ index.d.ts
@@ -4,20 +4,26 @@ declare module "react-date-picker" {
   export default function DatePicker(props: DatePickerProps): JSX.Element;
 
   export interface DatePickerProps extends CalendarProps {
+    calendarAriaLabel?: string;
     calendarClassName?: string | string[];
     calendarIcon?: JSX.Element | null;
     className?: string | string[];
+    clearAriaLabel?: string;
     clearIcon?: JSX.Element | null;
+    dayAriaLabel?: string;
     dayPlaceholder?: string;
     disabled?: boolean;
     format?: string;
     isOpen?: boolean;
+    monthAriaLabel?: string;
     monthPlaceholder?: string;
     name?: string;
-    onCalendarOpen?: () => void;
+    nativeInputAriaLabel?: string;
     onCalendarClose?: () => void;
+    onCalendarOpen?: () => void;
     required?: boolean;
     showLeadingZeros?: boolean;
+    yearAriaLabel?: string
     yearPlaceholder?: string;
   }
 }
