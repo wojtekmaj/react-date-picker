24f42514ea7ba4936185d633b941030ce995ce31
JounQin <admin@1stg.me>
2022-07-20 02:37:03 +0800

    chore: remove unnecessary `declare module` block in typings

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,32 @@
 import { CalendarProps } from 'react-calendar';
 
-declare module 'react-date-picker' {
-  export default function DatePicker(props: DatePickerProps): JSX.Element;
+export default function DatePicker(props: DatePickerProps): JSX.Element;
 
-  export interface DatePickerProps extends CalendarProps {
-    autoFocus?: boolean;
-    calendarAriaLabel?: string;
-    calendarClassName?: string | string[];
-    calendarIcon?: JSX.Element | null;
-    className?: string | string[];
-    clearAriaLabel?: string;
-    clearIcon?: JSX.Element | null;
-    closeCalendar?: boolean;
-    dayAriaLabel?: string;
-    dayPlaceholder?: string;
-    disabled?: boolean;
-    disableCalendar?: boolean;
-    format?: string;
-    isOpen?: boolean;
-    monthAriaLabel?: string;
-    monthPlaceholder?: string;
-    name?: string;
-    nativeInputAriaLabel?: string;
-    onCalendarClose?: () => void;
-    onCalendarOpen?: () => void;
-    openCalendarOnFocus?: boolean;
-    portalContainer?: HTMLElement;
-    required?: boolean;
-    showLeadingZeros?: boolean;
-    yearAriaLabel?: string;
-    yearPlaceholder?: string;
-  }
+export interface DatePickerProps extends CalendarProps {
+  autoFocus?: boolean;
+  calendarAriaLabel?: string;
+  calendarClassName?: string | string[];
+  calendarIcon?: JSX.Element | null;
+  className?: string | string[];
+  clearAriaLabel?: string;
+  clearIcon?: JSX.Element | null;
+  closeCalendar?: boolean;
+  dayAriaLabel?: string;
+  dayPlaceholder?: string;
+  disabled?: boolean;
+  disableCalendar?: boolean;
+  format?: string;
+  isOpen?: boolean;
+  monthAriaLabel?: string;
+  monthPlaceholder?: string;
+  name?: string;
+  nativeInputAriaLabel?: string;
+  onCalendarClose?: () => void;
+  onCalendarOpen?: () => void;
+  openCalendarOnFocus?: boolean;
+  portalContainer?: HTMLElement;
+  required?: boolean;
+  showLeadingZeros?: boolean;
+  yearAriaLabel?: string;
+  yearPlaceholder?: string;
 }
