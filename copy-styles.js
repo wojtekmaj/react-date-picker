34d4cf20bd090024b676cdf3a754dc792c936827
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-01-10 15:46:18 +0100

    Replace Less with CSS (#512)

--- copy-styles.js
+++ copy-styles.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-fs.copyFile('src/DatePicker.less', 'dist/DatePicker.less', (error) => {
+fs.copyFile('src/DatePicker.css', 'dist/DatePicker.css', (error) => {
   if (error) {
     throw error;
   }
   // eslint-disable-next-line no-console
-  console.log('DatePicker.less copied successfully.');
+  console.log('DatePicker.css copied successfully.');
 });
