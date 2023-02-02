a853356ad09a726559f6245a18a43a384597e84a
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-02-02 14:52:33 +0100

    Mark empty functions as intentionally empty

--- test-utils.js
+++ test-utils.js
@@ -1,9 +1,15 @@
 /* eslint-env jest */
 
 export function muteConsole() {
-  jest.spyOn(global.console, 'log').mockImplementation(() => {});
-  jest.spyOn(global.console, 'error').mockImplementation(() => {});
-  jest.spyOn(global.console, 'warn').mockImplementation(() => {});
+  jest.spyOn(global.console, 'log').mockImplementation(() => {
+    // Intentionally empty
+  });
+  jest.spyOn(global.console, 'error').mockImplementation(() => {
+    // Intentionally empty
+  });
+  jest.spyOn(global.console, 'warn').mockImplementation(() => {
+    // Intentionally empty
+  });
 }
 
 export function restoreConsole() {
