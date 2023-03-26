14026c5d4d023569335da606e06179324a708458
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-03-26 20:00:15 +0200

    Remove invalid useGrouping flag from options

--- src/DateInput.jsx
+++ src/DateInput.jsx
@@ -335,7 +335,7 @@ export default class DateInput extends PureComponent {
       const formatterOptions =
         getFormatterOptionsCache[name] ||
         (() => {
-          const options = { useGrouping: false, [name]: 'numeric' };
+          const options = { [name]: 'numeric' };
 
           getFormatterOptionsCache[name] = options;
 
