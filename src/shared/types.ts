d4ff681c181afca7bf86ba9a5513f56f8c14ed07
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-05-08 22:35:04 +0200

    Improve types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,13 +1,15 @@
+type Range<T> = [T, T];
+
 export type ClassName = string | null | undefined | (string | null | undefined)[];
 
 export type Detail = 'century' | 'decade' | 'year' | 'month';
 
 export type LooseValuePiece = string | Date | null;
 
-export type LooseValue = LooseValuePiece | [LooseValuePiece, LooseValuePiece];
+export type LooseValue = LooseValuePiece | Range<LooseValuePiece>;
 
 export type RangeType = 'century' | 'decade' | 'year' | 'month' | 'day';
 
 type ValuePiece = Date | null;
 
-export type Value = ValuePiece | [ValuePiece, ValuePiece];
+export type Value = ValuePiece | Range<ValuePiece>;
--- test/shared/types.ts
+++ test/shared/types.ts
@@ -1,5 +1,7 @@
+type Range<T> = [T, T];
+
 export type Detail = 'century' | 'decade' | 'year' | 'month';
 
 type LooseValuePiece = string | Date | null;
 
-export type LooseValue = LooseValuePiece | [LooseValuePiece, LooseValuePiece];
+export type LooseValue = LooseValuePiece | Range<LooseValuePiece>;
