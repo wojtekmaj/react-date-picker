655c994b12bc15d4a41870a3cc875c4afdb305b9
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-10-03 11:09:43 +0200

    Replace JSDom with Happy DOM

--- packages/react-date-picker/vitest.config.ts
+++ packages/react-date-picker/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    environment: 'happy-dom',
     server: {
       deps: {
         inline: ['vitest-canvas-mock'],
