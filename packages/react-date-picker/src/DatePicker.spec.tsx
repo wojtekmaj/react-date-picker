57f7606f214d1572c67e07887a2794b3a38b82dc
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-10-31 11:44:01 +0100

    Use act() from react-dom/test-utils

--- packages/react-date-picker/src/DatePicker.spec.tsx
+++ packages/react-date-picker/src/DatePicker.spec.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, it, vi } from 'vitest';
 import { userEvent } from 'vitest/browser';
 import { render } from 'vitest-browser-react';
-import { act } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
 import DatePicker from './DatePicker.js';
 
